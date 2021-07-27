ce81562218d3a4c695a9fc8cf80f65bd4f8848fd
Rezaul-Karim-Sujon <rksujon126706@gmail.com>
2021-07-27 22:49:51 +0600

    Enable migration code upload

---
+++ Enable Migration/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Enable_Migration.Startup))]
+namespace Enable_Migration
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
